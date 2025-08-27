db09e9ad20289ee2ef214f04140f6ee954ab86a2
JoaoVBonifácio <jboni2014@gmail.com>
2025-08-27 16:47:49 -0300

    chore: Finaliza e trava as configurações para o deploy

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Instala todas as dependências
 pip install -r requirements.txt
-
-# Coleta os arquivos estáticos
 python manage.py collectstatic --noinput
-
-# Aplica as migrações do banco de dados
 python manage.py migrate
